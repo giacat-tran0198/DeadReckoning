ea2a35d6465be65d1d5066a379cf2a9780ee6474
giacat-tran0198 <giacat.tran0198@gmail.com>
2021-10-22 11:41:06 +0200

    Initial commit

---
+++ app/src/main/java/com/mi12/deadreckoning/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mi12.deadreckoning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

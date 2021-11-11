06661a066484773860923bf5eef62de4c4cc9c86
giacat-tran0198 <giacat.tran0198@gmail.com>
2021-11-11 18:01:29 +0100

    update layout

--- app/src/main/java/com/mi12/deadreckoning/MainActivity.java
+++ app/src/main/java/com/mi12/deadreckoning/MainActivity.java
@@ -1,8 +1,8 @@
 package com.mi12.deadreckoning;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 

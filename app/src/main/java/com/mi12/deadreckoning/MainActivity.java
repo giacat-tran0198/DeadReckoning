f11b34defd20c0734b1a5adebd8b1cb850ff301e
giacat-tran0198 <giacat.tran0198@gmail.com>
2021-11-11 21:52:59 +0100

    update click button colors

--- app/src/main/java/com/mi12/deadreckoning/MainActivity.java
+++ app/src/main/java/com/mi12/deadreckoning/MainActivity.java
@@ -1,14 +1,41 @@
 package com.mi12.deadreckoning;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button buttonStart;
+    Button buttonReset;
+    TextView textStep;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find item
+        buttonStart = findViewById(R.id.buttonStart);
+        buttonReset = findViewById(R.id.buttonReset);
+        textStep = findViewById(R.id.textStep);
+
+        // Set event
+        buttonStart.setOnClickListener(v -> {
+            buttonStart.setSelected(!buttonStart.isSelected());
+            onChangeTextButtonStart();
+        });
+        buttonReset.setOnClickListener(v -> {
+            buttonStart.setSelected(false);
+            onChangeTextButtonStart();
+        });
+
+    }
+
+
+    private void onChangeTextButtonStart() {
+        buttonStart.setText(getString(buttonStart.isSelected() ? R.string.stop : R.string.start));
     }
 }
